5f0258a65f326358d6d4323cefc96d6fbb70efba
unknown <ivancas84@gmail.com>
2020-01-31 16:11:24 -0300

    cambiar forma de clonar objetos

--- src/gen/class/data-definition/_Storage.php
+++ src/gen/class/data-definition/_Storage.php
@@ -16,9 +16,9 @@ class EntityDataDefinition_Storage extends GenerateEntity {
   protected function start(){
     $this->string .= "  storage(row: { [index: string]: any }){
     if(!row) return;
-    var rowCloned = Object.assign({}, row);
+    var rowCloned = JSON.parse(JSON.stringify(row))
     /**
-     * se clona el objeto para poder eliminar atributos a medida que se procesa y no alterar la referencia original
+     * se realiza un 'deep clone' del objeto para poder eliminar atributos a medida que se procesa y no alterar la referencia original
      */
 ";
   }
