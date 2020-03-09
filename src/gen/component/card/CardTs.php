ee7b6b97f8dff2d085a006694765bf6c7a967d63
ivan <ivancas84@gmail.com>
2020-03-09 10:25:57 -0300

    simplificar componente card, eliminar constructor y atributos innecesarios

--- src/gen/component/card/CardTs.php
+++ src/gen/component/card/CardTs.php
@@ -11,41 +11,14 @@ class GenCardTs extends GenerateFileEntity {
   }
 
   protected function generateCode(){
-    $this->start();
-    $this->ngOnOnit();
-    $this->end();
-  }
-
-  protected function start(){
     $this->string .= "import { Component } from '@angular/core';
-import { DataDefinitionService } from '@service/data-definition/data-definition.service';
 import { CardComponent } from '@component/card/card.component';
-import { isEmptyObject } from '@function/is-empty-object.function';
-import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-" . $this->entity->getName("xx-yy") . "-card',
   templateUrl: './" . $this->entity->getName("xx-yy") . "-card.component.html',
 })
-export class " . $this->entity->getName("XxYy") . "CardComponent extends CardComponent {
-
-  readonly entityName = '" . $this->entity->getName() . "';
-
-  constructor(protected dd: DataDefinitionService) {
-    super(dd);
-  }
-
-";
-  }
-
-  protected function ngOnOnit(){
-    require_once("gen/component/card/_ngOnInit.php");
-    $gen = new GenCardTs_ngOnInit($this->entity);
-    $this->string .= $gen->generate();
-  }
-
-  protected function end(){
-    $this->string .= "}
+export class " . $this->entity->getName("XxYy") . "CardComponent extends CardComponent { }
 ";
   }
 }
