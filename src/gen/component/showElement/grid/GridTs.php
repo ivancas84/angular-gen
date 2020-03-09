7128bd76ef0224d0d6bee4ce0d1e58457c7c7374
ivan <ivancas84@gmail.com>
2020-03-09 12:24:12 -0300

    simplificar interfaces asociadas a showElement

--- src/gen/component/showElement/grid/GridTs.php
+++ src/gen/component/showElement/grid/GridTs.php
@@ -11,43 +11,15 @@ class GenGridTs extends GenerateFileEntity {
   }
 
   protected function generateCode(){
-    $this->import();
-    $this->start();
-    //$this->ngOnOnit();
-    $this->end();
-  }
-
-  protected function start(){
-    $this->string .= "@Component({
+    $this->string .= "import { Component } from '@angular/core';
+import { ShowElementComponent } from '@component/show-element/show-element.component';
+    
+@Component({
   selector: 'app-" . $this->entity->getName("xx-yy") . "-grid',
   templateUrl: './" . $this->entity->getName("xx-yy") . "-grid.component.html',
 })
-export class " . $this->entity->getName("XxYy") . "GridComponent extends ShowElementComponent {
+export class " . $this->entity->getName("XxYy") . "GridComponent extends ShowElementComponent { }
 
-  readonly entityName = '" . $this->entity->getName() . "';
-
-  constructor(protected dd: DataDefinitionService) {
-    super();
-  }
-
-";
-  }
-
-  protected function import(){
-    require_once("gen/component/showElement/_import.php");
-    $gen = new GenShowElementTs_import($this->entity);
-    $this->string .= $gen->generate();
-  }
-
-
-  protected function ngOnOnit(){
-    require_once("gen/component/showElement/_ngOnInit.php");
-    $gen = new GenShowElementTs_ngOnInit($this->entity);
-    $this->string .= $gen->generate();
-  }
-
-  protected function end(){
-    $this->string .= "}
 ";
   }
 }
--- src/gen/component/showElement/table/TableTs.php
+++ src/gen/component/showElement/table/TableTs.php
@@ -11,44 +11,16 @@ class GenTableTs extends GenerateFileEntity {
   }
 
   protected function generateCode(){
-    $this->import();
-    $this->start();
-    //$this->ngOnOnit();
-    $this->end();
-  }
+    $this->string .= "import { Component } from '@angular/core';
+import { ShowElementComponent } from '@component/show-element/show-element.component';
 
-  protected function start(){
-    $this->string .= "@Component({
+@Component({
   selector: 'app-" . $this->entity->getName("xx-yy") . "-table',
   templateUrl: './" . $this->entity->getName("xx-yy") . "-table.component.html',
 })
-export class " . $this->entity->getName("XxYy") . "TableComponent extends ShowElementComponent {
-
-  readonly entityName = '" . $this->entity->getName() . "';
-
-  constructor(protected dd: DataDefinitionService) {
-    super();
-  }
-
+export class " . $this->entity->getName("XxYy") . "TableComponent extends ShowElementComponent { }
 ";
   }
 
-  protected function import(){
-    require_once("gen/component/showElement/_import.php");
-    $gen = new GenShowElementTs_import($this->entity);
-    $this->string .= $gen->generate();
-  }
-
-
-  protected function ngOnOnit(){
-    require_once("gen/component/showElement/_ngOnInit.php");
-    $gen = new GenShowElementTs_ngOnInit($this->entity);
-    $this->string .= $gen->generate();
-  }
-
-  protected function end(){
-    $this->string .= "}
-";
-  }
 }
 
