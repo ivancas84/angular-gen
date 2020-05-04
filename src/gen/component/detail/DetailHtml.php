49fa59f6849965f61c01f5040cd5685475bd7a24
unknown <ivancas84@gmail.com>
2020-05-04 00:07:20 -0300

    Corregir directorio

--- src/gen/component/detail/DetailHtml.php
+++ src/gen/component/detail/DetailHtml.php
@@ -7,7 +7,7 @@ class Gen_DetailHtml extends GenerateFileEntity {
 
   public function __construct(Entity $entity, $directorio = null){
     $file = $entity->getName("xx-yy") . "-detail.component.html";
-    if(!$directorio) $directorio = PATH_GEN . "tmp/component/detail/" . $entity->getName("xx-yy") . "-show/";
+    if(!$directorio) $directorio = PATH_GEN . "tmp/component/detail/" . $entity->getName("xx-yy") . "-detail/";
     parent::__construct($directorio, $file, $entity);
   }
 
