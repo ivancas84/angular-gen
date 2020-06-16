32b332a508edf93358ef6bf8eadaafbaa69819e8
ivancas84 <ivancas84@gmail.com>
2020-06-16 00:55:23 -0300

    Desarrollo de formpick

--- src/component/formPick/FormPickTs.php
+++ src/component/formPick/FormPickTs.php
@@ -25,7 +25,7 @@ class GenFormPickTs extends GenerateFileEntity {
 
   protected function start(){
     $this->string .= "import { Component } from '@angular/core';
-import { FieldsetComponent } from '@component/fieldset/fieldset.component';
+import { FormPick } from '@component/form-pick/form-pick.component';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { DataDefinitionService } from '@service/data-definition/data-definition.service';
 import { ValidatorsService } from '@service/validators/validators.service';
@@ -38,7 +38,7 @@ import { isEmptyObject } from '@function/is-empty-object.function';
   selector: 'app-" . $this->entity->getName("xx-yy") . "-form-pick',
   templateUrl: './" . $this->entity->getName("xx-yy") . "-form-pick.component.html',
 })
-export class " . $this->entity->getName("XxYy") . "FormPick extends FormPick {
+export class " . $this->entity->getName("XxYy") . "FormPick extends FormPickComponent {
 
   readonly entityName: string = '" . $this->entity->getName() . "';
 
